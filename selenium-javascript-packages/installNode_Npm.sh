865d738b7eb2dc7c2b7c40e46fbed05f9a425f33
shaeen <shaeen@rapidtrade.biz>
2017-10-18 10:23:37 +0200

    selenium npm packages

---
+++ selenium-javascript-packages/installNode_Npm.sh
@@ -0,0 +1,8 @@
+# Step 1: Add Nodejs PPA
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+# Step 2: Install Nodejs and NPM
+sudo apt-get install nodejs
+# Step 3: Check Node.js and NPM Version
+node -v 
+npm -v 
---
+++ selenium-javascript-packages/seleniumLocal.sh
@@ -0,0 +1,5 @@
+sudo npm install selenium-webdriver@2.53.3
+sudo npm install chromedriver
+sudo npm install mocha
+sudo npm install chai
+sudo npm install chai-as-promised
