0aa6186d6273dcd5e6a5660343f9ca48b786199e
Don-Sbu <don@rapidtrade.biz>
2018-01-11 13:01:16 +0200

    lite-server package

--- selenium-javascript-packages/installNode_Npm.sh
+++ selenium-javascript-packages/installNode_Npm.sh
@@ -1,8 +1,6 @@
-# Step 1: Add Nodejs PPA
-sudo apt-get install python-software-properties
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-# Step 2: Install Nodejs and NPM
-sudo apt-get install nodejs
-# Step 3: Check Node.js and NPM Version
+# Step 1: Install Node and Npm
+sudo apt install nodejs-legacy
+sudo apt install npm
+# Step 2: Check Node.js and NPM Version
 node -v 
 npm -v 
---
+++ selenium-javascript-packages/lite-server.sh
@@ -0,0 +1,2 @@
+sudo npm install lite-server
+sudo npm install lite-server -g
