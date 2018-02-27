92da49672f8aaa4c7852ecbd34baab49a5c5266f
Shaeen Kumalaseelan <shaeen@rapidtrade.biz>
2018-02-27 12:57:36 +0200

    changes

--- selenium-javascript-packages/seleniumGlobal.sh
+++ selenium-javascript-packages/seleniumGlobal.sh
@@ -1,5 +1,5 @@
-sudo npm install -g selenium-webdriver@2.53.3
-sudo npm install -g chromedriver
+sudo npm install -g selenium-webdriver
+#sudo npm install -g chromedriver
 sudo npm install -g mocha
 sudo npm install -g chai
 sudo npm install -g chai-as-promised
---
+++ whatsapp.sh
@@ -0,0 +1,4 @@
+sudo wget https://www.thefanclub.co.za/sites/all/modules/pubdlcnt/pubdlcnt.php?file=https://www.thefanclub.co.za/sites/default/files/public/downloads/whatsapp-webapp_1.0_all.deb&nid=200
+sudo dpkg -i whatsapp-webapp_1.0_all.deb
+sudo -apt-get install -f
+
