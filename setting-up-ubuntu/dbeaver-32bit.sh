b45df792a41876ba3ddf03f0f86cf0a26773de3a
Shaeen <shaeen@rapidtrade.biz>
2018-06-05 14:45:38 +0200

    force install

--- setting-up-ubuntu/dbeaver-32bit.sh
+++ setting-up-ubuntu/dbeaver-32bit.sh
@@ -3,3 +3,4 @@ wget https://dbeaver.jkiss.org/files/dbeaver-ce_latest_i386.deb
 # install dbeaver
 # wait a while then search for dbeaver
 sudo dpkg -i dbeaver*.deb
+sudo apt-get install -f -y
--- setting-up-ubuntu/dbeaver-64bit.sh
+++ setting-up-ubuntu/dbeaver-64bit.sh
@@ -3,3 +3,4 @@ wget https://dbeaver.jkiss.org/files/dbeaver-ce_latest_amd64.deb
 # install dbeaver
 # wait a while then search for dbeaver
 sudo dpkg -i dbeaver*.deb
+sudo apt-get install -f -y
